00cbc6cbfaaa91f86d1530a40fbbfab7a61ddce8
ujwal6-eng <anandujwal6@gmail.com>
2025-08-24 12:46:37 +0530

    Create a Personalized Greeting

---
+++ Task2.py
@@ -0,0 +1,9 @@
+# Take first name input
+first_name = input("Enter your first name: ")
+# Take last name input
+last_name = input("Enter your last name: ")
+# Concatenate to create full name
+full_name = first_name + " " + last_name
+# Print Personalize greeting
+print("Hello, " + full_name + "! Welcome to Python program.")
+
